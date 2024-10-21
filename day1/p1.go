9cef175f11d0b1cf5e3620e204f39d366017c89d
Jeff Gonis <jeffgonis@meta.com>
2024-10-21 10:36:56 -0600

    First day puzzle

--- day1/p1.go
+++ day1/p1.go
@@ -1,7 +1,49 @@
 package day1
 
-import "fmt"
+import (
+	"strconv"
+)
 
-func p1() {
-	fmt.Println("hello world")
+func P1(input string) (int, error) {
+	digits, err := stringToNumSlice(input)
+	if err != nil {
+		return 0, err
+	}
+
+	sum := findSum(digits, 1)
+	return sum, nil
+}
+
+func P2(input string) (int, error) {
+	digits, err := stringToNumSlice(input)
+	if err != nil {
+		return 0, err
+	}
+	lookahead := len(digits) / 2
+	sum := findSum(digits, lookahead)
+
+	return sum, nil
+}
+
+func stringToNumSlice(input string) ([]int, error) {
+	digits := make([]int, 0)
+	for _, digit_rune := range input {
+		digit, err := strconv.Atoi(string(digit_rune))
+		if err != nil {
+			return nil, err
+		}
+		digits = append(digits, digit)
+	}
+	return digits, nil
+}
+
+func findSum(digits []int, lookahead int) int {
+	sum := 0
+	for index, digit := range digits {
+		nextIndex := (index + lookahead) % len(digits)
+		if digit == digits[nextIndex] {
+			sum += digit
+		}
+	}
+	return sum
 }
--- day1/p1_test.go
+++ day1/p1_test.go
@@ -1,9 +1,148 @@
 package day1
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+	"testing"
+)
 
 func TestP1(t *testing.T) {
+	testCases := []struct {
+		input string
+		want  int
+	}{
+		{
+			input: "1122",
+			want:  3,
+		},
+		{
+			input: "1111",
+			want:  4,
+		},
+		{
+			input: "1234",
+			want:  0,
+		},
+		{
+			input: "91212129",
+			want:  9,
+		},
+	}
+	for caseNumber, testCase := range testCases {
+		t.Run("Test case "+strconv.Itoa(caseNumber), func(t *testing.T) {
+			got, _ := P1(testCase.input)
+			if got != testCase.want {
+				t.Errorf("want %d, got %d", testCase.want, got)
+			}
+		})
+	}
+}
+
+func TestP1RealInput(t *testing.T) {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		t.Errorf("could not read input file: %v", err)
+	}
+	trimmedInput := strings.TrimSpace(string(input))
+	output, err := P1(trimmedInput)
+	if err != nil {
+		t.Errorf("could not run p1: %v", err)
+	}
+	fmt.Println(output)
 }
 
 func TestP2(t *testing.T) {
+	testCases := []struct {
+		input string
+		want  int
+	}{
+		{
+			input: "1212",
+			want:  6,
+		},
+		{
+			input: "1221",
+			want:  0,
+		},
+		{
+			input: "123425",
+			want:  4,
+		},
+		{
+			input: "123123",
+			want:  12,
+		},
+		{
+			input: "12131415",
+			want:  4,
+		},
+	}
+
+	for caseNumber, testCase := range testCases {
+		t.Run("Test case "+strconv.Itoa(caseNumber), func(t *testing.T) {
+			got, _ := P2(testCase.input)
+			if got != testCase.want {
+				t.Errorf("given: %v, want %d, got %d", testCase.input, testCase.want, got)
+			}
+		})
+	}
+}
+
+func TestP2RealInput(t *testing.T) {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		t.Errorf("could not read input file: %v", err)
+	}
+	trimmedInput := strings.TrimSpace(string(input))
+	output, err := P2(trimmedInput)
+	if err != nil {
+		t.Errorf("could not run p1: %v", err)
+	}
+	fmt.Println(output)
+}
+
+func TestStringToNumSlice(t *testing.T) {
+	testCases := []struct {
+		input     string
+		want      []int
+		wantError bool
+	}{
+		{
+			input:     "",
+			want:      []int{},
+			wantError: false,
+		},
+		{
+			input:     "1",
+			want:      []int{1},
+			wantError: false,
+		},
+		{
+			input:     "123",
+			want:      []int{1, 2, 3},
+			wantError: false,
+		},
+		{
+			input:     "123a",
+			want:      nil,
+			wantError: true,
+		},
+	}
+	for caseNumber, testCase := range testCases {
+		t.Run("Test case "+strconv.Itoa(caseNumber), func(t *testing.T) {
+			got, err := stringToNumSlice(testCase.input)
+			if err != nil && !testCase.wantError {
+				t.Errorf("want no error, got %v", err)
+			}
+			if err == nil && testCase.wantError {
+				t.Errorf("want error, got no error")
+			}
+			if !slices.Equal(got, testCase.want) {
+				t.Errorf("want %v, got %v", testCase.want, got)
+			}
+		})
+	}
 }

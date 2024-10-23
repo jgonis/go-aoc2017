a4fc04ab77b3c9d32a2a6948dfb266b951da9fd5
Jeff Gonis <jeffgonis@meta.com>
2024-10-23 12:13:33 -0600

    Day 3 problems

--- day3/day3.go
+++ day3/day3.go
@@ -1,5 +1,105 @@
 package day3
 
-func P1(input string) int {
-	return 0
+import (
+	"image"
+	"math"
+)
+
+func moveRight(currentPoint image.Point) image.Point {
+	return image.Pt(currentPoint.X+1, currentPoint.Y)
+}
+func moveLeft(currentPoint image.Point) image.Point {
+	return image.Pt(currentPoint.X-1, currentPoint.Y)
+}
+func moveUp(currentPoint image.Point) image.Point {
+	return image.Pt(currentPoint.X, currentPoint.Y+1)
+}
+func moveDown(currentPoint image.Point) image.Point {
+	return image.Pt(currentPoint.X, currentPoint.Y-1)
+}
+
+func P1(input int) int {
+	return generateSpiral(func(previousCellValue int, currentPosition image.Point) int {
+		return previousCellValue + 1
+	}, func(cellValue int, currentPosition image.Point) (int, bool) {
+		if cellValue == input {
+			return int(math.Abs(float64(currentPosition.X)) + math.Abs(float64(currentPosition.Y))), true
+		}
+		return 0, false
+	})
+}
+
+func P2(input int) int {
+	cells := map[image.Point]int{}
+	cells[image.Pt(0, 0)] = 1
+	return generateSpiral(func(previousCellValue int, currentPosition image.Point) int {
+		newCellValue := 0
+		neighbors := generateNeighbors(currentPosition)
+		for _, neighbor := range neighbors {
+			if value, present := cells[neighbor]; present {
+				newCellValue += value
+			}
+		}
+		cells[currentPosition] = newCellValue
+		return newCellValue
+	},
+		func(cellValue int, currentPosition image.Point) (int, bool) {
+			if cellValue > input {
+				return cellValue, true
+			}
+			return 0, false
+		})
+}
+
+func generateSpiral(valueCalculator func(int, image.Point) int, exitCondition func(int, image.Point) (int, bool)) int {
+	currentPoint := image.Pt(0, 0)
+	n := 1
+	cellValue := 1
+	rightCount := 0
+	upCount := 0
+	leftCount := 0
+	downCount := 0
+	for {
+		if value, shouldExit := exitCondition(cellValue, currentPoint); shouldExit {
+			return value
+		}
+
+		switch {
+		case rightCount < n:
+			rightCount += 1
+			currentPoint = moveRight(currentPoint)
+			cellValue = valueCalculator(cellValue, currentPoint)
+		case upCount < n:
+			upCount += 1
+			currentPoint = moveUp(currentPoint)
+			cellValue = valueCalculator(cellValue, currentPoint)
+		case leftCount < n+1:
+			leftCount += 1
+			currentPoint = moveLeft(currentPoint)
+			cellValue = valueCalculator(cellValue, currentPoint)
+		case downCount < n+1:
+			downCount += 1
+			currentPoint = moveDown(currentPoint)
+			cellValue = valueCalculator(cellValue, currentPoint)
+		default:
+			n += 2
+			rightCount = 0
+			upCount = 0
+			leftCount = 0
+			downCount = 0
+		}
+	}
+}
+
+func generateNeighbors(currentPoint image.Point) []image.Point {
+	neighbors := make([]image.Point, 0)
+	neighbors = append(neighbors, image.Pt(currentPoint.X+1, currentPoint.Y))
+	neighbors = append(neighbors, image.Pt(currentPoint.X+1, currentPoint.Y+1))
+	neighbors = append(neighbors, image.Pt(currentPoint.X, currentPoint.Y+1))
+	neighbors = append(neighbors, image.Pt(currentPoint.X-1, currentPoint.Y+1))
+	neighbors = append(neighbors, image.Pt(currentPoint.X-1, currentPoint.Y))
+	neighbors = append(neighbors, image.Pt(currentPoint.X-1, currentPoint.Y-1))
+	neighbors = append(neighbors, image.Pt(currentPoint.X, currentPoint.Y-1))
+	neighbors = append(neighbors, image.Pt(currentPoint.X+1, currentPoint.Y-1))
+	return neighbors
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -1,16 +1,81 @@
 package day3
 
-import "testing"
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
 
 func TestP1(t *testing.T) {
-	input := `17  16  15  14  13
-18   5   4   3  12
-19   6   1   2  11
-20   7   8   9  10
-21  22  23---> ...`
-	want := 0
-	got := P1(input)
-	if want != got {
-		t.Errorf("want %d, got %d", want, got)
+	testCases := []struct {
+		input int
+		want  int
+	}{
+		{
+			input: 1,
+			want:  0,
+		},
+		{
+			input: 12,
+			want:  3,
+		},
+		{
+			input: 23,
+			want:  2,
+		},
+		{
+			input: 1024,
+			want:  31,
+		},
 	}
+	for _, testCase := range testCases {
+		t.Run("Test case for value: "+strconv.Itoa(testCase.input), func(t *testing.T) {
+			got := P1(testCase.input)
+			if got != testCase.want {
+				t.Errorf("want %d, got %d", testCase.want, got)
+			}
+		})
+	}
+}
+
+func TestP1RealInput(t *testing.T) {
+	output := P1(325489)
+	fmt.Println(output)
+}
+
+func TestP2(t *testing.T) {
+	testCases := []struct {
+		input int
+		want  int
+	}{
+		{
+			input: 4,
+			want:  5,
+		},
+		{
+			input: 11,
+			want:  23,
+		},
+		{
+			input: 26,
+			want:  54,
+		},
+		{
+			input: 147,
+			want:  304,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run("Test case for value: "+strconv.Itoa(testCase.input), func(t *testing.T) {
+			got := P2(testCase.input)
+			if got != testCase.want {
+				t.Errorf("want %d, got %d", testCase.want, got)
+			}
+		})
+	}
+}
+
+func TestP2RealInput(t *testing.T) {
+	output := P2(325489)
+	fmt.Println(output)
 }
